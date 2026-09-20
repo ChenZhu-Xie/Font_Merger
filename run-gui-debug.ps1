4d7c5768c61577f19aa7f094613b21a39b83b48c
Xcz <294302704@qq.com>
2026-09-21 01:43:50 +0800

    Hide dedicated console for GUI launchers

--- run-gui-debug.ps1
+++ run-gui-debug.ps1
@@ -6,6 +6,43 @@ param(
 
 $ErrorActionPreference = "Stop"
 
+function Hide-DedicatedConsoleWindow {
+    if ($env:OS -ne "Windows_NT") {
+        return
+    }
+
+    if (-not ("FontMerger.ConsoleWindow" -as [type])) {
+        Add-Type -TypeDefinition @"
+using System;
+using System.Runtime.InteropServices;
+
+namespace FontMerger {
+    public static class ConsoleWindow {
+        [DllImport("kernel32.dll")]
+        public static extern IntPtr GetConsoleWindow();
+
+        [DllImport("kernel32.dll")]
+        public static extern uint GetConsoleProcessList(uint[] processList, uint processCount);
+
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool ShowWindow(IntPtr windowHandle, int command);
+    }
+}
+"@
+    }
+
+    $consoleProcesses = [uint[]]::new(2)
+    if ([FontMerger.ConsoleWindow]::GetConsoleProcessList($consoleProcesses, 2) -eq 1) {
+        $consoleWindow = [FontMerger.ConsoleWindow]::GetConsoleWindow()
+        if ($consoleWindow -ne [IntPtr]::Zero) {
+            [void][FontMerger.ConsoleWindow]::ShowWindow($consoleWindow, 0)
+        }
+    }
+}
+
+Hide-DedicatedConsoleWindow
+
 $repoRoot = $PSScriptRoot
 $buildScript = Join-Path $repoRoot "build-windows.ps1"
 $exePath = Join-Path $repoRoot "dist\local-debug\font-merger-gui-windows-x64-debug\font-merger-gui-windows-x64-debug.exe"
