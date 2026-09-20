b931e710f92f16f7e47982d98a2288d33a024b97
Xcz <294302704@qq.com>
2026-09-21 01:11:23 +0800

    chore: release v2.3.3

--- Font_Merger.py
+++ Font_Merger.py
@@ -28,7 +28,7 @@ from fontTools.ttLib import TTCollection, TTFont, newTable
 from fontTools.ttLib.scaleUpem import scale_upem
 from fontTools.varLib.instancer import instantiateVariableFont
 
-VERSION = "2.3.2"
+VERSION = "2.3.3"
 LOG = logging.getLogger("font-merger")
 
 COLLECTION_MAGIC = b"ttcf"
